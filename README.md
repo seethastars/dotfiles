21084724782adbfc58325ec9647fa53018c50701
Unseen <seethastars@gmail.com>
2024-09-30 11:11:53 +0000

    New dotfiles

---
+++ README.md
@@ -0,0 +1,100 @@
+<!-- Installation Instructions -->
+## Hyprland Setup
+
+To install and set up the Hyprland dotfiles, follow these steps:
+
+<small> 
+
+> **Note:** The hyprland.sh works better using a minimal install of Arch linux using the archinstall script.
+
+</small>
+
+1. Clone the repository:
+
+    ```sh
+    git clone https://github.com/seethastars/dotfiles.git
+    ```
+
+2. Change to the repository directory:
+
+    ```sh
+    cd dotfiles/
+    ```
+
+3. Make the install script executable:
+
+    ```sh
+    sudo chmod +x ./hyprland.sh
+    ```
+    
+4. Run the install script:
+
+    ```sh
+    ./hyprland.sh
+    ```
+   
+5. Enjoy Hyprland!
+
+---
+
+<div align="center">
+
+### Hyprfuture
+![image](assets/hyprfuture.png)
+
+### Hyprmocha
+![image](assets/hyprmocha.png)
+
+### Hyprvertical
+![image](assets/hyprvertical.png)
+
+</div>
+
+## Keybinds
+
+<div align="center">
+
+| Mapping                 | Action                                           |
+| ----------------------- | ------------------------------------------------ |
+| `Super + Return`        | Open Terminal                                    |
+| `Super + Q`             | Close Window                                     |
+| `Super + W`             | Open Web Browser - (Librewolf)                   |
+| `Super + E`             | Open File Manager - (Thunar)                     |
+| `Super + R`             | Open App Launcher - (Wofi)                       |
+| `Super + L`             | Exec lockscreen - (Hyprlock)                     |
+| `Super + B`             | Restart Waybar                                   |
+| `Super + N`             | Change wallpaper                                 |
+| `Super + M`             | Exit Hyprland                                    |
+
+<small>
+
+**Note:** The hyprland dots are **Work in Progress** (WIP).
+
+</small>
+
+---
+
+</div>
+
+## Other programs that i use
+
+- ![Yazi](https://github.com/sxyazi/yazi) - (CLI File Manager)
+- ![Cava](https://github.com/karlstav/cava) - (Audio Visualizer)
+- ![Neovim](https://neovim.io/) - (Text Editor)
+- ![MPV](https://mpv.io/) - (Media Player)
+
+## Xfce Setup
+
+I made a simple script similar to the hyprland.sh one for installing xfce much faster if i want to use it as my DE.
+
+Installing Xfce is easy as:
+
+1. Make the script executable:
+    ```sh 
+    sudo chmod +x ./xfce.sh
+    ```
+2. And run the script:
+
+    ```sh
+    ./xfce.sh
+    ```
---
+++ hyprland.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+
+set -e
+
+RC='\e[0m'
+RED='\e[31m'
+YELLOW='\e[33m'
+GREEN='\e[32m'
+BLUE='\e[34m'
+
+u="$USER"
+
+while :
+do
+  read -p "Hi $u, do you want to install Hyprland dependecies? (Y/n) " resp      # user response
+
+    if [[ $resp == "N" ]] || [[ $resp == "n" ]]; then 
+    echo -e "${BLUE}Ok, Have a great day :)${RC}"
+    exit 1
+
+    elif [[ $resp == "Y" ]] || [[ $resp == "y" ]]; then
+    echo -e "${YELLOW}Installing packages...${RC}"
+
+      # customize packages you want to install
+    sudo pacman -S hyprland qt5-wayland qt6-wayland xdg-desktop-portal-hyprland xdg-desktop-portal-gtk polkit-kde-agent hyprpaper hyprlock hypridle waybar wofi foot starship nwg-look kvantum-qt5 qt6ct thunar thunar-archive-plugin gvfs gvfs-gphoto2 gvfs-afc xarchiver imagemagick ristretto pavucontrol brightnessctl wl-clipboard unzip unrar p7zip qbittorrent gamemode grim slurp htop vlc network-manager-applet alsa-utils dosfstools ntfsprogs papirus-icon-theme gnome-themes-extra ttf-dejavu ttf-jetbrains-mono-nerd ttf-firacode-nerd adobe-source-han-sans-jp-fonts noto-fonts-emoji
+
+    echo -e "${GREEN}Base packages installed.${RC}"
+    echo -e "${YELLOW}Installing paru as AUR helper...${RC}"
+    git clone https://aur.archlinux.org/paru-bin.git
+    cd paru-bin/
+    makepkg -si
+    cd ..
+    rm -rf paru-bin/
+    echo -e "${GREEN}AUR helper installed.${RC}"
+
+    echo -e "${YELLOW}Installing AUR packages...${RC}"
+    paru -S librewolf-bin
+    echo -e "${GREEN}AUR packages installed.${RC}"
+
+    echo -e "${YELLOW}Cloning wallpapers repo...${RC}"
+    git clone https://github.com/seethastars/wallpapers
+    mkdir -p Pictures
+    mv wallpapers/Wallpapers Pictures/ && mv Pictures/ ~/
+    rm -rf wallpapers/
+
+    read -p "What theme do you want? (1)=Hyprblack (2)=Hyprmocha (3)=Hyprvertical " choice   #user choice
+        if [[ $choice == 1 ]]; then
+
+          echo -e "${YELLOW}Copying hyprblack dotfiles into ~/.config...${RC}"
+          cd hyprblack/
+          cp -r hypr/ waybar/ wofi/ foot/ fastfetch/ starship.toml ~/.config/ && mv bashrc .bashrc && cp -r .bashrc ~/
+          echo -e "${GREEN}Hyprblack dotfiles copied.${RC}"
+          echo -e "${BLUE}\nEnjoy Hyprland!${RC}"
+          exit 1
+
+        elif [[ $choice == 2 ]]; then 
+          echo -e "${YELLOW}Copying hyprmocha dotfiles into ~/.config...${RC}"
+          cd hyprmocha/
+          cp -r hypr/ waybar/ wofi/ foot/ fastfetch/ starship.toml ~/.config/ && mv bashrc .bashrc && cp -r .bashrc ~/
+          echo -e "${GREEN}Hyprmocha dotfiles copied.${RC}"
+          echo -e "${BLUE}\nEnjoy Hyprland! :3${RC}"
+          exit 1
+
+        elif [[ $choice == 3 ]]; then 
+          echo -e "${YELLOW}Copying hyprvertical dotfiles into ~/.config...${RC}"
+          cd hyprtop/
+          cp -r hypr/ waybar/ wofi/ foot/ fastfetch/ starship.toml ~/.config/ && mv bashrc .bashrc && cp -r .bashrc ~/
+          echo -e "${GREEN}Hyprvertical dotfiles copied.${RC}"
+          echo -e "${BLUE}\nEnjoy Hyprland!${RC}"
+          exit 1
+
+        else
+          echo -e "${RED}Please type 1 or 2 to choose a theme. (^C to quit.)${RC}"
+        fi
+
+      else
+        echo -e "${RED}Please type Y for confirm or N for decling (^C to quit.)${RC}"
+    fi 
+done
