5b7a32339589b3426fa7508cbcf890121add2432
seethastars <starsinsorrow@gmail.com>
2024-10-02 16:38:22 +0000

    Adjusted syntax

--- xfce.sh
+++ xfce.sh
@@ -12,7 +12,7 @@ u="$USER"
 
 while :
 do
-  read -p "Hi $u, do you want to install XFCE dependecies? (Y/n) " resp      # user response
+  read -p "Hi $u, do you want to install XFCE dependecies? (Y/n) " resp   # user response
 
     if [[ $resp == "N" ]] || [[ $resp == "n" ]]; then 
     exit 1
@@ -21,26 +21,26 @@ do
     echo -e "${YELLOW}Installing packages...${RC}"
 
       # customize packages you want to install
-    sudo pacman -S xorg xorg-xinit xfce4 xfce4-goodies xf86-video-amdgpu xarchiver unzip unrar p7zip htop mpv network-manager-applet alsa-utils gvfs dosfstools ntfsprogs papirus-icon-theme noto-fonts noto-fonts-emoji
+      sudo pacman -S xorg xorg-xinit xfce4 xfce4-goodies xf86-video-amdgpu xarchiver unzip unrar p7zip htop mpv network-manager-applet alsa-utils gvfs dosfstools ntfsprogs papirus-icon-theme noto-fonts noto-fonts-emoji
 
-    echo -e "${GREEN}Base packages installed.${RC}"
-    echo -e "${YELLOW}Installing paru as AUR helper...${RC}"
-    git clone https://aur.archlinux.org/paru-bin.git
-    cd paru-bin/
-    makepkg -si
-    cd ..
-    rm -rf paru-bin/
-    echo -e "${GREEN}AUR helper installed.${RC}"
+      echo -e "${GREEN}Base packages installed.${RC}"
+      echo -e "${YELLOW}Installing paru as AUR helper...${RC}"
+      git clone https://aur.archlinux.org/paru-bin.git
+      cd paru-bin/
+      makepkg -si
+      cd ..
+      rm -rf paru-bin/
+      echo -e "${GREEN}AUR helper installed.${RC}"
 
-    echo -e "${YELLOW}Installing AUR packages...${RC}"
-    paru -S librewolf-bin
-    echo -e "${GREEN}AUR packages installed.${RC}"
+      echo -e "${YELLOW}Installing AUR packages...${RC}"
+      paru -S librewolf-bin
+      echo -e "${GREEN}AUR packages installed.${RC}"
 
-    echo -e "${GREEN}All packages & dependecies installed.${RC}"
-    echo -e "${BLUE}\nEnjoy XFCE.${RC}"
-    exit 1
+      echo -e "${GREEN}All packages & dependecies installed.${RC}"
+      echo -e "${BLUE}\nEnjoy XFCE.${RC}"
+      exit 1
 
-    else
-    echo -e "${RED}Please type Y for confirm or N for decling (^C to quit.)${RC}"
-  fi 
+      else
+      echo -e "${RED}Please type Y for confirm or N for decling (^C to quit.)${RC}"
+    fi 
 done
