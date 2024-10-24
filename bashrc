d3cb997069f298ba73fe86b91c7f45c5ffeec95f
seethastars <starsinsorrow@gmail.com>
2024-10-24 18:08:20 +0200

    new clean look

---
+++ bashrc
@@ -0,0 +1,38 @@
+#
+# ~/.bashrc
+#
+
+# If not running interactively, don't do anything
+[[ $- != *i* ]] && return
+
+# Custom aliases
+alias ls='ls --color=auto'
+alias ll='ls -lah'
+alias grep='grep --color=auto'
+
+alias cp='cp -r'
+alias mv='mv -i'
+alias mkdir='mkdir -p'
+
+# Change directory aliases
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ....='cd../../..'
+
+# Alias's for archives
+alias mktar='tar -cvf'
+alias mkbz2='tar -cvjf'
+alias mkgz='tar -cvzf'
+alias untar='tar -xvf'
+alias unbz2='tar -xvjf'
+alias ungz='tar -xvzf'
+
+export EDITOR=nvim
+alias vim='nvim'
+
+PS1='[\u@\h \W]\$ '
+
+# Fastfetch startup
+fastfetch
+
+eval "$(starship init bash)"
--- hyprland.sh
+++ hyprland.sh
@@ -15,48 +15,37 @@ do
   read -p "Hi $u, do you want to install Hyprland dependecies? (Y/n) " resp      # user response
 
     if [[ $resp == "N" ]] || [[ $resp == "n" ]]; then 
-    echo -e "${BLUE}Ok, have a great day.${RC}"
-    exit 1
+      echo -e "${BLUE}Ok, have a great day.${RC}"
+      exit 1
 
     elif [[ $resp == "Y" ]] || [[ $resp == "y" ]]; then
-    echo -e "${YELLOW}Installing packages...${RC}"
+
+      echo -e "${YELLOW}Installing paru as AUR helper...${RC}"
+      git clone https://aur.archlinux.org/paru-bin.git
+      cd paru-bin/
+      makepkg -si
+      cd ..
+      rm -rf paru-bin/
+      echo -e "${GREEN}AUR helper installed.${RC}"
+
+      echo -e "${YELLOW}Installing AUR packages...${RC}"
 
       # customize packages you want to install
-    sudo pacman -S hyprland xdg-desktop-portal-hyprland xdg-desktop-portal-gtk polkit-kde-agent hyprpaper hyprlock hypridle wl-clipboard waybar wofi foot swaync nwg-look thunar thunar-archive-plugin gvfs gvfs-gphoto2 gvfs-afc xarchiver imagemagick ristretto pavucontrol brightnessctl unzip unrar p7zip transmission-gtk mpv htop alsa-utils dosfstools ntfsprogs papirus-icon-theme gnome-themes-extra ttf-jetbrains-mono-nerd adobe-source-han-sans-jp-fonts noto-fonts-emoji
+      paru -Syu hyprland xdg-desktop-portal-hyprland xdg-desktop-portal-gtk polkit-kde-agent hyprpaper hyprlock hypridle hyprshot wl-clipboard waybar rofi-wayland foot swaync wlogout librewolf-bin nwg-look thunar thunar-archive-plugin gvfs gvfs-gphoto2 gvfs-afc xarchiver imagemagick ristretto pavucontrol brightnessctl unzip unrar p7zip transmission-gtk mpv cava htop alsa-utils dosfstools ntfsprogs papirus-icon-theme gnome-themes-extra ttf-jetbrains-mono-nerd adobe-source-han-sans-jp-fonts noto-fonts-emoji
 
-    echo -e "${GREEN}Base packages installed.${RC}"
-    echo -e "${YELLOW}Installing paru as AUR helper...${RC}"
-    git clone https://aur.archlinux.org/paru-bin.git
-    cd paru-bin/
-    makepkg -si
-    cd ..
-    rm -rf paru-bin/
-    echo -e "${GREEN}AUR helper installed.${RC}"
-
-    echo -e "${YELLOW}Installing AUR packages...${RC}"
-    paru -S librewolf-bin hyprshot
-    echo -e "${GREEN}AUR packages installed.${RC}"
-
-    read -p "What theme do you want? (1)=Hyprnord (2)=Hyprfuture " choice   #user choice
-
-        if [[ $choice == 1 ]]; then
-          echo -e "${YELLOW}Copying hyprfuture dotfiles into ~/.config...${RC}"
-          cd hyprnord/ && cp -r * ~/.config/
-          echo -e "${GREEN}Hyprnord dotfiles copied.${RC}"
-          echo -e "${BLUE}\nEnjoy Hyprland!${RC}"
+      echo -e "${GREEN}AUR packages installed.${RC}"
+      
+      read -p "Do you want to copy the config files? (Y/n) "
+        
+        if [[ $resp == "N" ]] || [[ $resp == "n" ]]; then 
+          echo -e "${BLUE}Enjoy Hyprland.${RC}"
           exit 1
 
-        elif [[ $choice == 2 ]]; then 
-          cd hyprfuture/ && cp -r * ~/.config/
-          echo -e "${GREEN}Hyprfuture dotfiles copied.${RC}"
-          echo -e "${BLUE}\nEnjoy Hyprland!${RC}"
-          exit 1
-
-        else
-          echo -e "${RED}Please type 1 or 2 to choose a theme. (^C to quit.)${RC}"
+        elif [[ $resp == "Y" ]] || [[ $resp == "y" ]]; then
+          cd hyprchill/ && cp -r * ~/.config/ 
+          echo -e "${GREEN}Config files copied, Enjoy Hyprland."
         fi
-
-      else
-        echo -e "${RED}Please type Y for confirm or N for decling (^C to quit.)${RC}"
+    else
+      echo -e "${RED}Please type Y for confirm or N for decling (^C to quit.)${RC}"
     fi 
 done
