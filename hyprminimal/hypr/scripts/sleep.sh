fc3d0689d7d1ffc82ec502c167cb48f38e929aee
seethastars <starsinsorrow@gmail.com>
2024-12-02 22:32:26 +0100

    updated

--- hyprminimal/hypr/scripts/sleep.sh
+++
@@ -1,5 +0,0 @@
-hyprlock -w timeout 300 'hyprlock -f -c 000000' \
-            timeout 600 'hyprctl dispatch dpms off' \
-            resume 'hyprctl dispatch dpms on' \
-            timeout 900 'systemctl suspend' \
-            before-sleep 'hyprlock -f -c 000000' &
