07a85015362d516232d679763a7deb92c5979bfd
seethastars <starsinsorrow@gmail.com>
2025-04-07 16:28:02 -0400

    updated

--- setup.sh
+++ setup.sh
@@ -37,7 +37,7 @@ do
       hypr2_pkgs='xdg-desktop-portal-hyprland xdg-desktop-portal-gtk waybar'
       util_pkgs='man dunst htop gvfs gvfs-afc xarchiver p7zip unzip unrar alsa-utils dosfstools net-tools brightnessctl wl-clipboard'
       app_pkgs='librewolf-bin foot fuzzel thunar thunar-archive-plugin imagemagick ristretto mpv cmus neovim newsraft yt-dlp'
-      theme_pkgs='nwg-look papirus-icon-theme gnome-themes-extra ttf-jetbrains-mono-nerd noto-fonts noto-fonts-emoji'
+      theme_pkgs='nwg-look papirus-icon-theme gnome-themes-extra ttf-jetbrains-mono-nerd noto-fonts noto-fonts-cjk noto-fonts-emoji'
 
       paru -Syu $hypr_pkgs $hypr2_pkgs $util_pkgs $app_pkgs $theme_pkgs
 
