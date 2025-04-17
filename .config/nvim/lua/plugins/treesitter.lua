c341393ecef010b172d4624413025d5f26f72f40
seethastars <starsinsorrow@gmail.com>
2025-04-16 23:39:32 -0400

    updated

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -2,15 +2,18 @@ return {
   'nvim-treesitter/nvim-treesitter',
   build = ':TSUpdate',
 
-  opts = {
-    ensure_installed = {"c", "lua", "query", "markdown", "markdown_inline"},
+  config = function()
+    local config = require("nvim-treesitter.configs")
+    config.setup({
+      ensure_installed = {"c", "lua", "python", "css", "html", "query", "markdown", "markdown_inline"},
 
-    sync_installed = false,
-    auto_install = true,
+      sync_installed = false,
+      auto_install = true,
 
-    highlight_enable = { enable = true },
-    indent = { enable = true},
+      highlight_enable = { enable = true },
+      indent = { enable = true},
 
-    additional_vim_regex_highlighting = false,
-  },
+      additional_vim_regex_highlighting = false,
+    })
+  end
 }
